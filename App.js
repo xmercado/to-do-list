4f05c3fb575eb6f729eb710dbf9960f6775a43bc
Xavier Mercado <xmercado1576@gmail.com>
2021-10-06 13:00:58 -0700

    Add list, generate list, text input

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
-
-//Build the to do list from object of items
-
-// Definition of Interaction:
-// List item will appear within text area on the page when
-// user clicks button or presses enter
-
-// Assets:
-// List items from object
-
-// Expected outcome: 
-// Display list item on the screen and add to list.
-
+import { StyleSheet, Text, View, ScrollView, TextInput, Alert, Button } from 'react-native';
 
 export default function App() {
-  const [itemCount, setItemCount] = useState(2);
-  const [items, setItems] = useState({
-    1: 'first item',
-    2: 'second item',
-  });
-  const itemsArray = Object.entries(items);
+  const [items, setItems] = useState([
+    {todo: 'first item'},
+    {todo: 'second item'}
+  ]);
+  const [userInput, setUserInput] = useState('');
+
+  const generateList = items.map((item, index) => (
+      <View key={index}>
+        <Text>{item.todo}</Text>
+      </View>
+    )
+  );
+
+  const showAlert = () => {
+    console.log('SHOW ALERT');
+    Alert.alert(
+      'Invalid Task',
+      'Please enter valid string',
+      [{
+        text: 'OK',
+        style: 'cancel',
+      }])
+  };
+
+  const addToList = () => {
+    if (items == null) return;
+    setItems([...items, {todo: textInput}]);
+    setTextInput('');
+  };
+
+  const deleteFromList = () => {
+    // ? something with setTasks filter ?
+  };
+
+  const verifyInput = () => {
+    // if input is string
+    // check for illegal characters too, regex?
+    if (typeof textInput === 'string') addToList();
+    else showAlert();
+  };
 
   return (
     <View style={styles.container}>
-        <ScrollView>
-          {
-            itemsArray.map((item, index) => {
-              return (
-                <View key={item[0]} id={item[0]}>
-                  <Text>{item[1]}</Text>
-                </View>
-              );
-            })
-          }
-        </ScrollView> 
-        
-        {/* Text Input with Button 
-            Uses function that accepts user input task and adds to list
-        */}
-      <StatusBar style="auto" />
+      <Text style={styles.appTitle}>To Do List</Text>
+        <ScrollView style={styles.itemListContainer}>
+          {generateList}
+        </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.itemListInput}
+        onChangeText={text => setUserInput(text)}
+        onSubmitEditing={verifyInput}
+        value = {userInput}
+        placeholder='Type task here' />
+        <Button
+          title='Add to list' 
+          onPress={verifyInput}/>
+        <Button
+          title='Clear input'
+          onPress={() => setUserInput()}
+          color='red'/>
+      </View>
     </View>
   );
 }
@@ -50,6 +73,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  appTitle: {
+    fontSize: '30px',
+    padding: 15,
+  },
+  itemListContainer: {
+    width: '100%',
+    padding: 15,
+  },
+  inputContainer: {
+    width: '100%',
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  itemListInput: {
+    height: 25,
+    width: '100%',
+    border: 'solid 1px',
+    borderRadius: 5,
+    lineHeight: '1em',
+    padding: '0.5em',
+    margin: 'auto',
   },
 });
